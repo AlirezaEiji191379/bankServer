32ef123cab9c1ea2f1ee551365f9ef6b3e5b46e9
Alireza <alirezaeiji151379@gmail.com>
2020-07-24 00:08:29 -0700

    الان عین اونا شد!

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,18 +3,22 @@ import Controller.RequestProcessor;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Server {
 
     public static void main(String[] args) {
         try {
+            int id=999;
             ServerSocket serverSocket=new ServerSocket(8080);
             Socket socket=null;
             while (true){
                 socket=serverSocket.accept();
+                id++;
+                System.out.println("client with id "+id+" connected!");
                 DataOutputStream dataOutputStream=new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
                 DataInputStream dataInputStream=new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-                new ClientHandler(socket,dataOutputStream,dataInputStream).start();
+                new ClientHandler(socket,dataOutputStream,dataInputStream,id).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -23,13 +27,15 @@ public class Server {
 
 
 static class ClientHandler extends Thread{
+   int clientId=0;
    DataInputStream dataInputStream;
    DataOutputStream dataOutputStream;
    Socket socket;
-   public ClientHandler(Socket socket,DataOutputStream dataOutputStream,DataInputStream dataInputStream){
+   public ClientHandler(Socket socket,DataOutputStream dataOutputStream,DataInputStream dataInputStream , int clientId){
        this.socket=socket;
        this.dataInputStream=dataInputStream;
        this.dataOutputStream=dataOutputStream;
+       this.clientId=clientId;
    }
     @Override
     public void run() {
@@ -49,6 +55,9 @@ static class ClientHandler extends Thread{
                    socket.close();
                    return;
                }
+           } catch (SocketException socketException){
+               System.out.println("client with id "+clientId+" disconnected!");
+               return;
            } catch (IOException e) {
                e.printStackTrace();
            }
